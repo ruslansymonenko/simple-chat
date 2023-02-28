cb1df9fa147a628823d3ca361d97a31cd1b9d15a
Ruslan Symonenko <symonenkors@gmail.com>
2023-02-28 13:07:21 +0200

    create html structure, and added css

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html' )
+});
+
+app.use(express.static(__dirname + '/assets'));
+
 http.listen(3000, () => {
   console.log('Server is working...');
 })
